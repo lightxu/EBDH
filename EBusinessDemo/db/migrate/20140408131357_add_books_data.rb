8ece51d71bdc2991f84e063f981940ed317ec5af
Seraph <fmx1992@gmail.com>
2014-04-08 07:11:27 -0700

    insert data

--- EBusinessDemo/db/migrate/20140408131357_add_books_data.rb
+++ EBusinessDemo/db/migrate/20140408131357_add_books_data.rb
@@ -1,9 +1,11 @@
 class AddBooksData < ActiveRecord::Migration
   def up
-    File.readlines("/file/path").each do |line|
+    File.readlines("/tmp/final.csv").each do |line|
       items = lines.split(',')
       print items
-      Books.create title: items[0]
+      year = items[4].split('年')
+      month = year[1].split('月')
+      Books.create title: items[0], price: items[1].to_f(), publisher: items[2], author: items[3], publish_date: data.new(year[0].to_i(), month[0].to_i()), amazon_link: items[5]
     end
   end
 
@@ -12,3 +14,4 @@ class AddBooksData < ActiveRecord::Migration
   end
 
 end
+
